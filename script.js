8df3ca03dd557265c740322a13b82e99d23244f3
Leinine Joseph <leninejoe03@gmail.com>
2024-04-17 16:20:06 +0530

    Final Commit

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function toggleTheme() {
 
       const Delements = document.querySelectorAll('#D_theme')
       Delements.forEach(element => {
-        element.style.color = '#555555';
+        element.style.color = 'black';
       });
 
       const img_elements = document.querySelectorAll('.icon');
